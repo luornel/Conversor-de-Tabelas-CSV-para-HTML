cc6679a167703c908fcff63e52f748cc9cdda76b
Luã Ornelas <103165178+luornel@users.noreply.github.com>
2023-10-30 21:58:35 -0300

    Update conversor_de_tabelas_csv_para_html.py

--- conversor_de_tabelas_csv_para_html.py
+++ conversor_de_tabelas_csv_para_html.py
@@ -1,29 +1,36 @@
 import pandas as pd
 import re
+from datetime import datetime
+
+def converter_formato_data(data):
+    try:
+        data_obj = datetime.strptime(data, '%m/%d/%Y')
+        data_formatada = data_obj.strftime('%d/%m/%Y')
+        return data_formatada
+    except ValueError:
+        return 'Data inválida'
 
 def gerar_tabela_html(entradas):
     dados = {'Posição': [], 'Data': [], 'Nome': []}
 
     for entrada in entradas:
-
         partes = re.match(r'"(.*?)"\s+"(.*?)"\s+"(.*?)"', entrada)
         if not partes:
             return 'Formato de entrada inválido. Por favor, insira no formato "posição" "data" "nome".'
 
         posicao, data, nome = partes.groups()
+        data_formatada = converter_formato_data(data)
         dados['Posição'].append(posicao)
-        dados['Data'].append(data)
+        dados['Data'].append(data_formatada)
         dados['Nome'].append(nome)
 
     df = pd.DataFrame(dados)
-
     tabela_html = df.to_html(index=False, escape=False)
-
     return tabela_html
 
 entradas = [
-'"1" "30/10/2023" "Luã Ornelas"',
-'"2" "30//10/2023" "João Silva"'
+'"1" "10/31/2023" "Luã Ornelas"',
+'"2" "10/31/2023" "João Silva"'
 ]
 tabela_html = gerar_tabela_html(entradas)
 print(tabela_html)
